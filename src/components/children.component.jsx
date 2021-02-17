1fb010ede3a515a18aefdaf1e28bd1808a1f002d
pargolgivechi <pargolgivechi@gmail.com>
2021-02-17 18:24:56 +0100

    fixing images presentation

--- src/components/children.component.jsx
+++ src/components/children.component.jsx
@@ -10,14 +10,22 @@ const Children = ({ childData }) => (
           <h3>Title: {childData.entity.title && childData.entity.title}</h3>
           <h5>Text: {childData.entity.text && childData.entity.text.value} </h5>
           <p>{childData.entity.image && childData.entity.image.entity.media.url}</p>
-          <div className='image' style={{ backgroundImage: `url(${childData.entity.image && childData.entity.image.entity.media[0].url})` }}>TEST IMAGE</div>
+          {childData.entity.image ?
+            <div className='image' style={{ backgroundImage: `url(${childData.entity.image && childData.entity.image.entity.media[0].url})` }}>
+              NO IMAGE
+            </div>
+            : ''}
         </div>
         :
         <div className='column'>
           <h3>Title: {childData.entity.title && childData.entity.title}</h3>
           <h5>Text: {childData.entity.text && childData.entity.text.value} </h5>
           <p>{childData.entity.image && childData.entity.image.entity.media.url}</p>
-          <div className='image' style={{ backgroundImage: `url(${childData.entity.image && childData.entity.image.entity.media[0].url})` }}>TEST IMAGE</div>
+          {childData.entity.image ?
+            <div className='image' style={{ backgroundImage: `url(${childData.entity.image && childData.entity.image.entity.media[0].url})` }}>
+              NO IMAGE
+            </div>
+            : ''}
         </div>
     }
   </Fragment>
--- src/components/parent.component.jsx
+++ src/components/parent.component.jsx
@@ -4,14 +4,18 @@ import './parent.styles.scss';
 
 import Children from './children.component';
 
-const Parent = ({containerData, data}) => (
+const Parent = ({ containerData, data }) => (
   <div className='parent'>
-  <h1>Container number: {containerData.entity.id}</h1>
-  
-  {data.map(paragraph => 
-     paragraph.entity.component !== 'ParagraphContainer' &&  paragraph.entity.layout &&  paragraph.entity.layout.split('"')[9] === containerData.entity.uuid ? 
-     <Children key={paragraph.entity.uuid} childData={paragraph}/> : ''
-     
+    <h1>Container number: {containerData.entity.id}</h1>
+
+    {data.map(paragraph =>
+      paragraph.entity.component !== 'ParagraphContainer' &&
+        paragraph.entity.layout &&
+        paragraph.entity.layout.split('"')[9] === containerData.entity.uuid
+        ?
+        <Children key={paragraph.entity.uuid} childData={paragraph} />
+        : ''
+
     )}
   </div>
 )
